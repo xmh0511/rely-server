e85564c12c53e98386662d53242b9c8e368c8f28
xmh0511 <970252187@qq.com>
2023-09-14 16:35:05 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use rand::Rng;
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
+    io::AsyncReadExt,
     net::{
         tcp::{OwnedReadHalf, OwnedWriteHalf},
         TcpListener,
     },
+    task::JoinHandle,
 };
 
 use tun::TunPacket;
 
+pub mod write_all;
+
 mod mock_reply;
 
+#[allow(dead_code)]
 enum Message {
     Add((u16, OwnedWriteHalf)),
     Data((u16, Vec<u8>)),
     Remove(u16),
-	#[allow(dead_code)]
-	Mock((u16,Vec<u8>))
+    Mock((u16, Vec<u8>)),
 }
 
 async fn read_body(len: u16, reader: &mut OwnedReadHalf) -> Result<Vec<u8>, std::io::Error> {
@@ -49,13 +52,21 @@ async fn read_body(len: u16, reader: &mut OwnedReadHalf) -> Result<Vec<u8>, std:
     }
 }
 
-async fn find_another(map:& mut HashMap<u16,OwnedWriteHalf>,me:u16)->Option<& mut OwnedWriteHalf>{
-	for i in map{
-		if *i.0 != me{
-			return Some(i.1);
-		}
-	}
-	return None;	
+async fn find_another(
+    map: &HashMap<u16, Arc<OwnedWriteHalf>>,
+    me: u16,
+) -> Option<&Arc<OwnedWriteHalf>> {
+    for i in map {
+        if *i.0 != me {
+            return Some(i.1);
+        }
+    }
+    return None;
+}
+
+enum AsyncMessage {
+    Add((String, JoinHandle<()>)),
+    Remove(String),
 }
 
 #[tokio::main]
@@ -63,7 +74,24 @@ async fn main() {
     let listen = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<Message>();
-    let task = tokio::spawn(async move {
+    let (tx_async, mut rx_async) = tokio::sync::mpsc::unbounded_channel::<AsyncMessage>();
+
+    let establish_task = tokio::spawn(async move {
+        let mut save_tasks = HashMap::new();
+        loop {
+            match rx_async.recv().await {
+                Some(AsyncMessage::Add((id, task))) => {
+                    save_tasks.insert(id, task);
+                }
+                Some(AsyncMessage::Remove(id)) => {
+                    println!("task complete, so removed");
+                    save_tasks.remove(&id);
+                }
+                None => {}
+            }
+        }
+    });
+    let write_tasks = tokio::spawn(async move {
         let mut map = HashMap::new();
         loop {
             //println!("execute");
@@ -72,36 +100,50 @@ async fn main() {
                     if map.len() >= 2 {
                         continue;
                     }
-                    map.insert(num, writer);
+                    map.insert(num, Arc::new(writer));
                 }
                 Some(Message::Data((num, buff))) => {
                     println!("receive data from {num}:\n{buff:?}");
-					match find_another(& mut map,num).await{
-						Some(writer)=>{
-							let mut w_buf = Vec::new();
-							let len = (buff.len() as u16).to_be_bytes();
-							w_buf.extend_from_slice(&len);
-							w_buf.extend_from_slice(&buff);
-							writer.write_all(&w_buf).await.unwrap();
-						}
-						None=>{}
-					}
+                    match find_another(&map, num).await {
+                        Some(writer) => {
+                            let writer = Arc::clone(writer);
+                            let uuid = uuid::Uuid::new_v4().to_string();
+                            let tx_async_copy = tx_async.clone();
+                            let _ = tx_async.send(AsyncMessage::Add((
+                                uuid.clone(),
+                                tokio::spawn(async move {
+                                    write_all::write_all(writer, buff).await;
+                                    let _ = tx_async_copy.send(AsyncMessage::Remove(uuid));
+                                }),
+                            )));
+                        }
+                        None => {}
+                    }
                 }
                 Some(Message::Remove(num)) => {
-					println!("remove {num}");
+                    println!("remove {num}");
                     map.remove(&num);
                 }
-				Some(Message::Mock((num,buff)))=>{
-					println!("mock buff {buff:?}");
-					let writer = map.get_mut(&num).unwrap();
-					let time = rand::thread_rng().gen_range(100..500);
-					tokio::time::sleep(std::time::Duration::from_millis(time)).await;
-					mock_reply::parse_tun_packet(Some(Ok(TunPacket::new(buff))), writer).await;
-					//writer.write_all(src)
-				}
+                Some(Message::Mock((num, buff))) => {
+                    println!("mock buff:\n {buff:?}");
+                    let writer = map.get(&num).unwrap().clone();
+                    let uuid = uuid::Uuid::new_v4().to_string();
+                    let tx_async_copy = tx_async.clone();
+                    let _ = tx_async.send(AsyncMessage::Add((
+                        uuid.clone(),
+                        tokio::spawn(async move {
+                            let time = rand::thread_rng().gen_range(100..3000);
+                            tokio::time::sleep(std::time::Duration::from_millis(time)).await;
+                            mock_reply::parse_tun_packet(Some(Ok(TunPacket::new(buff))), writer)
+                                .await;
+                            let _ = tx_async_copy.send(AsyncMessage::Remove(uuid));
+                        }),
+                    )));
+                    //writer.write_all(src)
+                }
                 None => {
-					return;
-				}
+                    return;
+                }
             };
         }
     });
@@ -109,7 +151,7 @@ async fn main() {
     let mut index = 0;
     while let Ok((stream, _)) = listen.accept().await {
         let (mut reader, writer) = stream.into_split();
-        tx.send(Message::Add((index, writer))).unwrap();
+        let _ = tx.send(Message::Add((index, writer)));
         let tx = tx.clone();
         tokio::spawn(async move {
             let mut len_buf = [0u8; 2];
@@ -119,7 +161,7 @@ async fn main() {
                     Ok(size) => {
                         println!("read in comming {size}");
                         if size == 0 {
-                            tx.send(Message::Remove(index)).unwrap();
+                            let _ = tx.send(Message::Remove(index));
                             return;
                         }
                         read_header_len += size;
@@ -131,10 +173,10 @@ async fn main() {
                                 Ok(buf) => {
                                     //println!("ready body:\n {buf:?}");
                                     //tx.send(Message::Data((index, buf))).unwrap();
-									tx.send(Message::Mock((index, buf))).unwrap();
+                                    let _ = tx.send(Message::Mock((index, buf)));
                                 }
                                 Err(_) => {
-                                    tx.send(Message::Remove(index)).unwrap();
+                                    let _ = tx.send(Message::Remove(index));
                                     return;
                                 }
                             }
@@ -143,7 +185,7 @@ async fn main() {
                         }
                     }
                     Err(_) => {
-                        tx.send(Message::Remove(index)).unwrap();
+                        let _ = tx.send(Message::Remove(index));
                         return;
                     }
                 }
@@ -151,5 +193,6 @@ async fn main() {
         });
         index += 1;
     }
-	task.await.unwrap();
+    write_tasks.await.unwrap();
+    establish_task.await.unwrap();
 }
--- src/mock_reply.rs
+++ src/mock_reply.rs
@@ -1,30 +1,25 @@
 use packet::{builder::Builder, icmp, ip, Packet};
-use tun::{AsyncDevice, Configuration, TunPacket, TunPacketCodec};
-use std::io::Error;
+use std::{io::Error, sync::Arc};
+use tun::TunPacket;
 
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::{
-        tcp::{OwnedReadHalf, OwnedWriteHalf},
-        TcpListener,
-    },
-};
+use tokio::net::tcp::OwnedWriteHalf;
 
-async fn write_packet_to_socket(packet: TunPacket, stream: &mut OwnedWriteHalf) {
-    let buff = packet.get_bytes();
-    let len = buff.len() as u16;
-    let bytes = len.to_be_bytes();
-    let mut write_buffer = Vec::new();
-    write_buffer.extend_from_slice(&bytes);
-    write_buffer.extend_from_slice(buff);
-    stream.write(&write_buffer).await.unwrap();
-}
+use super::write_all;
+
+// async fn write_packet_to_socket(packet: TunPacket, stream: &mut OwnedWriteHalf) {
+//     let buff = packet.get_bytes();
+//     let len = buff.len() as u16;
+//     let bytes = len.to_be_bytes();
+//     let mut write_buffer = Vec::new();
+//     write_buffer.extend_from_slice(&bytes);
+//     write_buffer.extend_from_slice(buff);
+//     stream.write(&write_buffer).await.unwrap();
+// }
 
 pub async fn parse_tun_packet(
     packet: Option<Result<TunPacket, Error>>,
-    stream: &mut OwnedWriteHalf,
-) where
-{
+    stream: Arc<OwnedWriteHalf>,
+) {
     match packet {
         Some(packet) => match packet {
             Ok(raw_pkt) => match ip::Packet::new(raw_pkt.get_bytes()) {
@@ -36,36 +31,32 @@ pub async fn parse_tun_packet(
                             // packet is icmp echo
                             match icmp.echo() {
                                 Ok(icmp) => {
+                                    let reply = ip::v4::Builder::default()
+                                        .id(0x42)
+                                        .unwrap()
+                                        .ttl(64)
+                                        .unwrap()
+                                        .source(pkt.destination())
+                                        .unwrap()
+                                        .destination(pkt.source())
+                                        .unwrap()
+                                        .icmp()
+                                        .unwrap()
+                                        .echo()
+                                        .unwrap()
+                                        .reply()
+                                        .unwrap()
+                                        .identifier(icmp.identifier())
+                                        .unwrap()
+                                        .sequence(icmp.sequence())
+                                        .unwrap()
+                                        .payload(icmp.payload())
+                                        .unwrap()
+                                        .build()
+                                        .unwrap();
 
-									let reply = ip::v4::Builder::default()
-									.id(0x42)
-									.unwrap()
-									.ttl(64)
-									.unwrap()
-									.source(pkt.destination())
-									.unwrap()
-									.destination(pkt.source())
-									.unwrap()
-									.icmp()
-									.unwrap()
-									.echo()
-									.unwrap()
-									.reply()
-									.unwrap()
-									.identifier(icmp.identifier())
-									.unwrap()
-									.sequence(icmp.sequence())
-									.unwrap()
-									.payload(icmp.payload())
-									.unwrap()
-									.build()
-									.unwrap();
-
-									write_packet_to_socket(
-										TunPacket::new(reply),
-										stream,
-									)
-									.await;
+									write_all::write_all(stream, reply).await;
+                                    //write_packet_to_socket(TunPacket::new(reply), stream).await;
                                     return;
                                 }
                                 _ => {}
