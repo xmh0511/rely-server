895b31d996ed9c4265c02d205b65ded024fbfed3
xmh0511 <970252187@qq.com>
2023-09-14 10:19:54 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,139 @@
-use std::{collections::HashMap, sync::Arc, io::Write};
+use std::collections::HashMap;
 
 use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
-    net::{TcpListener, TcpStream},
-    task::JoinSet,
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpListener,
+    },
 };
 
-#[tokio::main]
-async fn main() {
-    let listen = TcpListener::bind("192.168.1.11:3000").await.unwrap();
+enum Message {
+    Add((u16, OwnedWriteHalf)),
+    Data((u16, Vec<u8>)),
+    Remove(u16),
+}
 
-    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<TcpStream>();
-    let task = tokio::spawn(async move {
-        let mut vec = Vec::new();
-        loop {
-            //println!("execute");
-            match rx.try_recv() {
-                Ok(stream) => vec.push(stream),
-                Err(e) => {
-                    match e {
-                        tokio::sync::mpsc::error::TryRecvError::Empty =>{
-
-                        },
-                        tokio::sync::mpsc::error::TryRecvError::Disconnected => {
-                            return;
-                        },
-                    }
+async fn read_body(len: u16, reader: &mut OwnedReadHalf) -> Result<Vec<u8>, std::io::Error> {
+    let len = len as usize;
+    let mut buf = Vec::new();
+    buf.resize(len as usize, b'\0');
+    let mut read_len = 0;
+    loop {
+        match reader.read(&mut buf[read_len..]).await {
+            Ok(size) => {
+                if size == 0 {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::ConnectionAborted,
+                        "",
+                    ));
                 }
-            };
-            for s in &vec {
-                let mut buff = [0u8;1024];
-                match s.try_read(&mut buff){
-                    Ok(s)=>{
-                        if s!=0{
-                            println!("{s:?}\n {buff:?}");
-                        }
-                    }
-                    Err(_)=>{}
+                read_len += size;
+                if read_len == len {
+                    return Ok(buf);
+                } else {
+                    continue;
                 }
             }
+            Err(e) => {
+                return Err(e);
+            }
         }
-    });
-    let mut join_set = JoinSet::new();
-    while let Ok((stream, addr)) = listen.accept().await {
-        tx.send(stream).unwrap();
-        join_set.spawn(async move {
-            // loop{
-            //     let mut len_buf = [0u8;2];
-            //     stream.readable().await;
-            //     match stream.try_read(& mut len_buf){
-            //         Ok(size)=>{
-            //             if size == 2{
-            //                 let len = u16::from_be_bytes(len_buf);
-            //                 let mut buffer = Vec::new();
-            //                 buffer.resize(len as usize, b'\0');
-            //                 match stream.try_read(& mut buffer){
-            //                     Ok(_)=>{
-            //                         println!("read out buffer:\n{addr:?}\n{buffer:?}");
-            //                         // if let Some(other_stream)  = find_other(& mut *map.lock().unwrap(),addr.to_string()){
-            //                         //     other_stream.write(&buffer).await.unwrap();
-            //                         // }
-
-            //                     }
-            //                     Err(_)=>{
-            //                         println!("read buffer error");
-            //                     }
-            //                 }
-            //             }else{
-            //                 continue;
-            //             }
-            //         }
-            //         Err(_)=>{}
-            //       }
-            // }
-        });
     }
 }
 
+async fn find_another(map:& mut HashMap<u16,OwnedWriteHalf>,me:u16)->Option<& mut OwnedWriteHalf>{
+	for i in map{
+		if *i.0 != me{
+			return Some(i.1);
+		}
+	}
+	return None;	
+}
 
-fn void_main(){
-    let mut socket = std::net::TcpStream::connect("192.168.1.11").unwrap();
-    let buff = b"123456789abcdefg";
-    socket.write_all(buff).unwrap();
+#[tokio::main]
+async fn main() {
+    let listen = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<Message>();
+    let task = tokio::spawn(async move {
+        let mut map = HashMap::new();
+        loop {
+            //println!("execute");
+            match rx.recv().await {
+                Some(Message::Add((num, writer))) => {
+                    if map.len() >= 2 {
+                        continue;
+                    }
+                    map.insert(num, writer);
+                }
+                Some(Message::Data((num, buff))) => {
+                    println!("receive data from {num}:\n{buff:?}");
+					match find_another(& mut map,num).await{
+						Some(writer)=>{
+							let mut w_buf = Vec::new();
+							let len = (buff.len() as u16).to_be_bytes();
+							w_buf.extend_from_slice(&len);
+							w_buf.extend_from_slice(&buff);
+							writer.write_all(&w_buf).await.unwrap();
+						}
+						None=>{}
+					}
+                }
+                Some(Message::Remove(num)) => {
+					println!("remove {num}");
+                    map.remove(&num);
+                }
+                None => {
+					return;
+				}
+            };
+        }
+    });
+    // let mut join_set = JoinSet::new();
+    let mut index = 0;
+    while let Ok((stream, _)) = listen.accept().await {
+        let (mut reader, writer) = stream.into_split();
+        tx.send(Message::Add((index, writer))).unwrap();
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            let mut len_buf = [0u8; 2];
+            let mut read_header_len = 0;
+            loop {
+                match reader.read(&mut len_buf[read_header_len..]).await {
+                    Ok(size) => {
+                        println!("read in comming {size}");
+                        if size == 0 {
+                            tx.send(Message::Remove(index)).unwrap();
+                            return;
+                        }
+                        read_header_len += size;
+                        if read_header_len == 2 {
+                            read_header_len = 0;
+                            let body_len = u16::from_be_bytes(len_buf);
+                            println!("body len {body_len}");
+                            match read_body(body_len, &mut reader).await {
+                                Ok(buf) => {
+                                    //println!("ready body:\n {buf:?}");
+                                    tx.send(Message::Data((index, buf))).unwrap();
+                                }
+                                Err(_) => {
+                                    tx.send(Message::Remove(index)).unwrap();
+                                    return;
+                                }
+                            }
+                        } else {
+                            continue;
+                        }
+                    }
+                    Err(_) => {
+                        tx.send(Message::Remove(index)).unwrap();
+                        return;
+                    }
+                }
+            }
+        });
+        index += 1;
+    }
+	task.await.unwrap();
 }
